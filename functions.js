f2aee78b56c998e2ac538b49b0322c958cc86cb8
Marco <marco.mayorga@gmail.com>
2023-02-06 20:50:34 -0600

    fix some comments

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 export function heightToMetric(heightFt, heightIn) {
-    // it is necessary to parse because height is submitted as a string
+    // It is necessary to parse because height is submitted as a string
     let heightFtFloat = parseFloat(heightFt);
     let heightInFloat = parseFloat(heightIn);
     let heightinInches = (heightFtFloat * 12) + heightInFloat;
@@ -8,7 +8,7 @@ export function heightToMetric(heightFt, heightIn) {
 }
 
 export function weightToMetric(weight) {
-    // it is necessary to parse because height is submitted as a string
+    // It is necessary to parse because height is submitted as a string
     let weightFloat = parseFloat(weight);
     let weightInkg = weightFloat * 0.45359237;
     return weightInkg;
@@ -16,14 +16,15 @@ export function weightToMetric(weight) {
 
 
 export function femaleTDEE(weight, height, age, activityLevel) {
-    // This is the mifflin St jeoor formula multiplied by activitylevel to get the Tdee
-    // tdee is the total daily energy expenditure of a person
+    // This is the Mifflin-St Jeor Equation multiplied by activitylevel to get the Tdee
+    // Tdee is the total daily energy expenditure of a person
     let tdee = activityLevel * (10 * weight) + (6.25 * height) - (5 * age) - 161;
     return tdee;
 }
 
 export function maleTDEE(weight, height, age, activityLevel) {
-    // mesurments are in metric so i have to convert
+    // This is the Mifflin-St Jeor Equation multiplied by activitylevel to get the Tdee
+    // Tdee is the total daily energy expenditure of a person
     let tdee = activityLevel * ((10 * weight) + (6.25 * height) - (5 * age) + 5);
     return tdee;
 }
@@ -40,6 +41,7 @@ export function macros(tdee, weight) {
     1g of carbs = 4 cals
         the rest of the calories is aloccated to this
     */
+
     let weightInPounds = parseFloat(weight.value);
 
     // Cals and grams of protein
